137b117b3b1f3d7f290e56866c4836e43474e7b8
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-02-08 13:57:13 +0300

    ADD BRANCH

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,11 @@
 class Like < ApplicationRecord
-end
+    belongs_to :author, class_name: 'User'
+    belongs_to :post
+    after_save :increment_likes_counter
+  
+    private
+  
+    def increment_likes_counter
+      post.increment!(:likes_counter)
+    end
+  end
