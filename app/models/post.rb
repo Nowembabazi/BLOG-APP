d135bc8eee2be8d79012c441903973528199ed2c
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-02-09 11:16:46 +0300

    Add tests to the validation branch

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   after_save :increment_posts_counter
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :LikesCounter, numericality: { greater_than_or_equal_to: 0 }
+  validates :CommentsCounter, numericality: { greater_than_or_equal_to: 0 }
+
   def increment_posts_counter
     author.increment!(:posts_counter)
   end
