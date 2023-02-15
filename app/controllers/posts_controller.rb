217d1f4a19eef550015cb53f5efdaa8e34a38bc0
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-02-15 18:39:54 +0300

    commit views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
+    def index
+      @user = User.find(params[:user_id])
+    end
+  
+    def show
+      @user = User.find(params[:user_id])
+      @post = Post.find(params[:id])
+    end
   end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-end
+  
