aa557e312d951135c21e636e5ce0202901ca7260
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-02-22 17:25:21 +0300

    add database

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_07_102605) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
@@ -33,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_102605) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
