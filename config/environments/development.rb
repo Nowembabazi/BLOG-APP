6a3c59cece6ac95ae8808a9e6ad05b098bbae3d9
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-02-23 11:55:14 +0300

    add gem file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Default URL for the Devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -68,3 +72,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
